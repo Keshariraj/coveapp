58046f115d921f16a1651adb8b7da28de4538b62
zaid kamil <xaidmetamorphos@gmail.com>
2022-05-27 16:58:30 +0530

    push karo khush raho

--- app/src/main/java/com/digi/coveapp/organizer/StaffEventListFragment.kt
+++ app/src/main/java/com/digi/coveapp/organizer/StaffEventListFragment.kt
@@ -31,10 +31,8 @@ class StaffEventListFragment : Fragment(), OnEventItemClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
